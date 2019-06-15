c63a21a9d821987e4ae6e78c3422c420887089fc
Pieter Bouwer <pieterbouwer@protonmail.com>
2019-06-15 15:14:54 +0200

    changed host IP to 127.0.0.1

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ qemu-system-arm -kernel kernel-qemu-4.14.79-stretch \
     -dtb versatile-pb.dtb -no-reboot \
     -serial stdio -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
     -drive "file=2019-04-08-raspbian-stretch-lite.img,index=0,media=disk,format=raw" \
-    -net 'user,hostfwd=tcp::5022-:22,guestfwd=tcp:10.0.2.200:1234-cmd:nc 192.168.0.100 8842' \
+    -net 'user,hostfwd=tcp::5022-:22,guestfwd=tcp:10.0.2.200:4029-cmd:nc 127.0.0.1 4029' \
     -net nic
